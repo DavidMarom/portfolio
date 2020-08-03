6150f19098b2c0b0450e28f552705ab126d35271
David Marom <davidmarom@hotmail.com>
2020-08-03 19:47:28 +0300

    progress2...

--- js/service.js
+++ js/service.js
@@ -1,9 +1,9 @@
 var portData = [{
         id: "sokoban",
         name: "Sokoban",
-        title: "Better push those boxes",
-        desc: "lorem ipsum lorem ipsum lorem ipsum",
-        url: "projs/sokoban",
+        title: "Minesweeper",
+        desc: "This game will blow your mind",
+        url: "https://davidmarom.github.io/Minesweeper/",
         publishedAt: 1448693940000,
         labels: ["Matrixes", "keyboard events"],
     },
@@ -38,7 +38,7 @@ function renderPortGrid(portData) {
     portData.map(function (obj,iDx) {
 
         HTMLstr += `<div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+          <a class="portfolio-link" data-toggle="modal" href="${obj.url}">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
@@ -47,8 +47,8 @@ function renderPortGrid(portData) {
             <img class="img-fluid" src="img/portfolio/0${iDx+1}-thumbnail.jpg" alt="">
           </a>
           <div class="portfolio-caption">
-            <h4>${portData[iDx].title}</h4>
-            <p class="text-muted">Illustration</p>
+            <h4>${obj.title}</h4>
+            <p class="text-muted">${obj.desc}</p>
           </div>
         </div>`;
     });

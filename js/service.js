d70a0ea5d03ff763e395b21bc96e1a61e9c88d3c
David Marom <davidmarom@hotmail.com>
2020-09-29 15:19:31 +0300

    project added

--- js/service.js
+++ js/service.js
@@ -1,48 +1,57 @@
 var portData = [{
-    id: "sokoban",
-    name: "Sokoban",
-    title: "Minesweeper",
-    desc: "This game will blow your mind... and legs...",
-    url: "https://davidmarom.github.io/Minesweeper/",
-    publishedAt: 1448693940000,
-    labels: ["a", "b"],
-  },
-  {
-    id: "b",
-    name: "b",
-    title: "Bookshop ERP",
-    desc: "Books inventory management system, Includes CRUD, sorting and language support",
-    url: "https://davidmarom.github.io/book-shop-erp/",
-    publishedAt: 1448693940000,
-    labels: ["", ""],
-  },
-  {
-    id: "c",
-    name: "c",
-    title: "Responsive Design",
-    desc: "An example for a 100% responsive website built from scratch",
-    url: "proj/misterBlogger/index.html",
-    publishedAt: 1448693940000,
-    labels: ["", ""],
-  },
-  {
-    id: "d",
-    name: "d",
-    title: "Meme Generator",
-    desc: "A meme generator. ",
-    url: "https://davidmarom.github.io/meme-generator/",
-    publishedAt: 0,
-    labels: ["", ""],
-  },
-  {
-    id: "e",
-    name: "e",
-    title: "Notes-Books App",
-    desc: "React app: an interactive notes app",
-    url: "https://davidmarom.github.io/appsus/#/note",
-    publishedAt: 0,
-    labels: ["", ""],
-  }
+  id: "sokoban",
+  name: "Sokoban",
+  title: "Minesweeper",
+  desc: "This game will blow your mind... and legs...",
+  url: "https://davidmarom.github.io/Minesweeper/",
+  publishedAt: 1448693940000,
+  labels: ["a", "b"],
+},
+{
+  id: "b",
+  name: "b",
+  title: "Bookshop ERP",
+  desc: "Books inventory management system, Includes CRUD, sorting and language support",
+  url: "https://davidmarom.github.io/book-shop-erp/",
+  publishedAt: 1448693940000,
+  labels: ["", ""],
+},
+{
+  id: "c",
+  name: "c",
+  title: "Responsive Design",
+  desc: "An example for a 100% responsive website built from scratch",
+  url: "proj/misterBlogger/index.html",
+  publishedAt: 1448693940000,
+  labels: ["", ""],
+},
+{
+  id: "d",
+  name: "d",
+  title: "Meme Generator",
+  desc: "A meme generator. ",
+  url: "https://davidmarom.github.io/meme-generator/",
+  publishedAt: 0,
+  labels: ["", ""],
+},
+{
+  id: "e",
+  name: "e",
+  title: "Notes-Books App",
+  desc: "React app: an interactive notes app",
+  url: "https://davidmarom.github.io/appsus/#/note",
+  publishedAt: 0,
+  labels: ["", ""],
+},
+{
+  id: "f",
+  name: "f",
+  title: "Activities Marketplace",
+  desc: "Health-e is a markeplace for life-style activities. Anyone can attend or organaize a public activity. [MongoDB, React]",
+  url: "https://healthy3.herokuapp.com/#/activity",
+  publishedAt: 0,
+  labels: ["", ""],
+}
 ]
 
 function renderPortGrid(portData) {
@@ -51,13 +60,13 @@ function renderPortGrid(portData) {
   portData.map(function (obj, iDx) {
 
     HTMLstr += `<div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${iDx+1}">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${iDx + 1}">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
-            <img class="img-fluid" src="img/portfolio/0${iDx+1}-thumbnail.jpg" alt="">
+            <img class="img-fluid" src="img/portfolio/0${iDx + 1}-thumbnail.jpg" alt="">
           </a>
           <div class="portfolio-caption">
             <h4>${obj.title}</h4>
@@ -76,7 +85,7 @@ function renderModals(portData) {
 
   portData.map(function (obj, iDx) {
 
-    HTMLstr += `<div class="portfolio-modal modal fade" id="portfolioModal${iDx+1}" tabindex="-1" role="dialog" aria-hidden="true">
+    HTMLstr += `<div class="portfolio-modal modal fade" id="portfolioModal${iDx + 1}" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="close-modal" data-dismiss="modal">
@@ -91,12 +100,23 @@ function renderModals(portData) {
                 <!-- Project Details Go Here -->
                 <h2>${obj.title}</h2>
                 <p class="item-intro text-muted">${obj.desc}</p>
-                <a href="${obj.url}" target=_blank"><img class="img-fluid d-block mx-auto" src="img/portfolio/0${iDx+1}-full.jpg" alt="${obj.title}"></a>
+                <a href="${obj.url}" target=_blank"><img class="img-fluid d-block mx-auto" src="img/portfolio/0${iDx + 1}-full.jpg" alt="${obj.title}"></a>
                 <p></p>
                 
+                <form action="${obj.url}" target="_blank">
+                <input class="btn btn-primary" type="submit" value="< Visit Website >" />
+              </form>
+
+              <p></p><p></p>
+
+
+
                 <button class="btn btn-primary" data-dismiss="modal" type="button">
                     <i class="fa fa-times"></i>
-                    Close Project</button>
+                    Close</button>
+
+                   
+                
               </div>
             </div>
           </div>
@@ -117,5 +137,5 @@ function sendMail() {
 
   var hrefStr = `https://mail.google.com/mail/?view=cm&fs=1&to=davidmarom.pro@gmail.com&su=${subEl}&body=${messEl}`;
   window.open(hrefStr, '_blank');
-  
+
 }
